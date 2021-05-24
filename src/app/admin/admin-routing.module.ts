3e18190ccac15b1c83f9dd6f2e182d94bb7c55d8
Juan <juan.frc.utn@gmail.com>
2021-05-24 02:04:08 -0300

    Creacion de ambiente de pre produccion y configuracion en angular.json

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -4,6 +4,7 @@ import { ProductFormComponent } from './components/product-form/product-form.com
 import { NavComponent} from '../admin/components/nav/nav.component';
 import { DashboardComponent} from '../admin/components/dashboard/dashboard.component';
 import { TableDataSource } from '../admin/components/table/table-datasource';
+import { TableComponent } from './components/table/table.component';
 const routes: Routes = [
 
   {
@@ -20,7 +21,7 @@ const routes: Routes = [
       },
       {
         path:'table',
-        component: TableDataSource
+        component: TableComponent
       }
     ]
   }
